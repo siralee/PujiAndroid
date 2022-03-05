b475258323a75c9dc3b85002fe36d72ed36daf69
DESKTOP-F6BM7PC\user <ihuirak617@gmail.com>
2022-03-05 16:35:14 +0900

    I', DAN

--- app/src/main/java/com/nasna/puji/main.java
+++ app/src/main/java/com/nasna/puji/main.java
@@ -8,5 +8,6 @@ public class main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        System.out.println("");
     }
 }
